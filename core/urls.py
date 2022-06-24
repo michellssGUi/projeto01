9a85200a39d0422395934129698f6d6fa2c238c2
micellssgui <michellss.gui@gmail.com>
2022-06-24 10:05:00 -0300

    Configurado models

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from views import index, produto
+from .views import index
 
 urlpatterns = [
     path('', index, name='index'),
-    path('produto', produto, name='produto')
+    #path('produto', produto, name='produto')
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import Produto
 
-# Create your views here.
+def index(request):
+    produtos = Produto.objects.all()
+    
+    context = {
+        'nome': 'Michel Souza Santana - GUi',
+        'curso': 'Programação Web Com Django Framework',
+        'fone': 'WhatsApp: (47) 9 9665-8601',
+        'produtos': produtos,
+    }
+    return render(request, 'index.html', context)
